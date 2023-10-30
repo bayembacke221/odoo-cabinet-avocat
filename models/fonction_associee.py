20a7412fcc441c96970dbb78e78ed5d50e9a29e6
bayembacke221 <mbackembaye74@gmail.com>
2023-10-30 13:13:17 +0000

    add models and views

--- models/fonction_associee.py
+++ models/fonction_associee.py
@@ -7,7 +7,6 @@ class FonctionAssociee(models.Model):
     liste_associe_ids = fields.One2many('avocat.associe', 'fonction_associee_id', string="Liste des Associés")
     nbr_associe = fields.Integer(compute='_compute_nbr_associe', store=True)
 
-    @api.depends('liste_associe_ids')
+    
     def _compute_nbr_associe(self):
-        for fonction in self:
-            fonction.nbr_associe = len(fonction.liste_associe_ids)
+        self.nbr_associe = self.env['avocat.associe'].search_count([('fonction_associee_id','=',self.id)])
